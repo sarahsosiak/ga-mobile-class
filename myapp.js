ef00916e206a3d1386a55329979610ac6a262c6a
sarahsosiak <ssosiak@gmail.com>
2015-03-08 14:24:15 -0700

    Finished project
    Completed class project

--- myapp.js
+++ myapp.js
@@ -1,58 +1,75 @@
 // BEGIN DAY 1.
 
 var gallery;
+var artistMap;
+var selectedArtistId;
 
 function setup() {
 	gallery = document.getElementById('myGallery');
-
 	gallery.addEventListener('drawer-open', handleOpen);
 	gallery.addEventListener('drawer-close', handleClose);
+	gallery.addEventListener('thumbnail-tap', handleTap);	
 
-	drawThumbnails();
-	
-	document.addEventListener('tap', function() {
-		gallery.openDrawer();
-	});
+	var sampleTrack = document.getElementById('sample-track');
+	sampleTrack.addEventListener('tap', handlePlayback);
 
+	fetchData();
 }
 
-function handleOpen() {
+function fetchData() {
+	$.getJSON('https://cardshark.firebaseio.com/artists.json', function (dagoods) {
+		artistMap = dagoods;
+		gallery.drawThumbnailsFromObject(artistMap);
+	});
+}
+
+function handlePlayback(){
+	var myAudio = document.getElementById('music-sample');
+	var playImage = document.getElementById('play');
+	var equalImage = document.getElementById('equalizer');
+
+	playImage.style.opacity = 0;
+
+	myAudio.addEventListener('canplay', function () {
+		equalImage.style.opacity = 1;
+	})
+
+	myAudio.addEventListener('pause', function () {
+		equalImage.style.opacity = 0;
+		playImage.style.opacity = 1;
+		console.log('stopped');
+	})
+
+	myAudio.src = artistMap[selectedArtistId].sampletrack.url;
+	myAudio.play();
+}
+
+function handleTap(e) {
+	selectedArtistId = e._args;
+	gallery.setDrawerBackground(artistMap[selectedArtistId].thumbnail);
+	gallery.openDrawer();
+}
+
+function handleOpen(e) {
+
+	console.log(selectedArtistId);
+
 	var artistName = document.getElementById('artist-name');
-	artistName.innerHTML = 'Lady Gaga';
+	artistName.innerHTML = artistMap[selectedArtistId].name;
 
 	var artistDescription = document.getElementById('artist-description');
-	artistDescription.innerHTML = "Glamorously gaudy, a self-made post-modern diva stitched together from elements of Madonna, David Bowie, and Freddie Mercury, Lady Gaga was the first true millennial superstar. Mastering the constant connection of the internet era, Gaga generated countless mini sensations through her style, her videos, and her music, cultivating a devoted audience she dubbed Little Monsters. But it wasn't just a cult that turned her 2008 manifesto The Fame into a self-fulfilling prophecy: Gaga crossed over into the mainstream, ushering out one pop epoch and kick-starting a new one, quickly making such turn-of-the-century stars as Christina Aguilera and Britney Spears seem old-fashioned, quite a trick for any artist to pull off, but especially impressive for an artist who specialized in repurposing the past -- particularly the '80s -- for present use, creating sustainable pop for a digital world.";
+	artistDescription.innerHTML = artistMap[selectedArtistId].description;
 
 	var mainContent = document.getElementById('main-content');
 	mainContent.style.opacity = 1;
 
 	var songTitle = document.getElementById('song');
-	songTitle.innerHTML = "Edge of Glory";
-
+	songTitle.innerHTML = artistMap[selectedArtistId].sampletrack.trackname;
 }
 
 function handleClose() {
 	var mainContent = document.getElementById('main-content');
 	mainContent.style.opacity = 0;
-
-}
-
-function drawThumbnails() {
-	
-	var gymclass = {
-		id: 'gymclassheroes',
-		thumbnail: 'resources/gymclass.jpg'
-
-	};
-
-	var katy = {
-		id: 'katyperry',
-		thumbnail: 'resources/katyperry.jpg'
-	};
-
-	var group = [katy];
-
-	myGallery.drawThumbnailsFromArray(group);
 }
 
 
